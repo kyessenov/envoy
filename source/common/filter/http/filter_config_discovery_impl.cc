fd35dc0610805498105b3e10244e934a495a949e
Kuat <kyessenov@users.noreply.github.com>
2020-08-05 17:04:49 -0700

    merge fix
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- source/common/filter/http/filter_config_discovery_impl.cc
+++ source/common/filter/http/filter_config_discovery_impl.cc
@@ -58,7 +58,11 @@ void DynamicFilterConfigProviderImpl::onConfigUpdate(Envoy::Http::FilterFactoryC
     auto prev_config = std::dynamic_pointer_cast<ThreadLocalConfig>(previous);
     prev_config->config_ = config;
     return previous;
-  });
+  }, [this, config]() {
+        // This happens after all workers have discarded the previous config so it can be safely
+        // deleted on the main thread by an update with the new config.
+        this->current_config_ = config;
+      });
 }
 
 FilterConfigSubscription::FilterConfigSubscription(
